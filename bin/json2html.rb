e6785377acd64a71701310e30ccf6b827b674468
Ruxton <ruxton@thefrontiergroup.com.au>
2013-10-16 15:42:35 +0800

    Method for building entities

--- bin/json2html.rb
+++ bin/json2html.rb
@@ -6,6 +6,19 @@ datapath = "data/beatmakers/"
 datafiles = "*.json"
 outputdir = "output/"
 
+def build_entity(file,output)
+  puts "Parsing #{file}"
+  str = File.read(file)
+  d = JSON.parse(str)
+  output << "<h2>"
+  output << d["name"]
+  output << "</h2>"
+  output << "<ul>"
+  d.delete_if{ |k,v| k == "name" }
+  d.each { |k,v| output << "<li><a href=\"#{v}\">#{k}</a></li>" }
+  output << "</ul>"
+end
+
 Dir.glob("#{datapath}*").select {|f| File.directory? f}.each do |dir|
   dirname = dir.split("/").last
   FileUtils.mkdir_p(outputdir)
@@ -15,16 +28,7 @@ Dir.glob("#{datapath}*").select {|f| File.directory? f}.each do |dir|
     datafilescount = Dir[datafilespath].length
     output << "<em>#{datafilescount} Beatmakers in list</em>"
     Dir.glob(datafilespath).each do |file|
-      puts "Parsing #{file}"
-      str = File.read(file)
-      d = JSON.parse(str)
-      output << "<h2>"
-      output << d["name"]
-      output << "</h2>"
-      output << "<ul>"
-      d.delete_if{ |k,v| k == "name" }
-      d.each { |k,v| output << "<li><a href=\"#{v}\">#{k}</a></li>" }
-      output << "</ul>"
+      build_entity(file,output)
     end
   ensure
     output.close
